d32837a699c5ef35938ae201b29e3b33c495d4fb
Gabriel Cardoso <gcardoso.w@gmail.com>
2023-08-08 21:14:16 +0200

    Update application.js

--- app/assets/javascripts/administrate-field-taggable/application.js
+++ app/assets/javascripts/administrate-field-taggable/application.js
@@ -1,9 +1,11 @@
 // ActsAsTaggableOn Administrate Form
 $(function() {
   $(".field-unit--acts-as-taggable .field-unit__field > input").each(function(elem) {
-    var $this = $(this);
-    var opts = $this.data('tag-options');
+      var $this = $(this);
+    var opts = $this.data('tag-options') == "493";
 
+
+    
     $this.selectize({
       delimiter: ", ",
       persist: false,
